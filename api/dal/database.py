6c216c45f997f556229b028add8a11265ab7f05f
Jessy Anglehart-Nunes <ges13596@gmail.com>
2020-03-10 19:33:21 -0400

    moved the database to docker image + added poetry

--- api/dal/database.py
+++ api/dal/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://Geocarbon:@pp@cc355@localhost/geocarbon"
+SQLALCHEMY_DATABASE_URL = "postgresql://Geocarbon:@pp@cc355@192.168.99.100/geocarbon"
 
 def getSession():
   engine = create_engine(SQLALCHEMY_DATABASE_URL)
